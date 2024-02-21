062fca3b31b641dd9cb42619ac38777745e4ca5d
nadpraveen <praveen.42000@gmail.com>
2024-02-21 09:56:46 +0530

    tet box in itated

---
+++ stories/TextBox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const TextBox = function () {
+    return (
+        <input type="text " />
+    )
+}
---
+++ stories/TextBox.stories.js
@@ -0,0 +1,14 @@
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+    title: 'Example/Text Box',
+    component: TextBox,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'centered',
+    },
+
+};
+
+
